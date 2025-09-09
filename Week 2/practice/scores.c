38c58fe77bf1a82a24d83f47179ef40f719dfa3b
Ti-De-08 <d.tirtha@op.iitg.ac.in>
2025-09-09 23:26:08 +0600

    Add files via upload

---
+++ Week 2/practice/scores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int scores1 = 71;
+    int scores2 = 72;
+    int scores3 = 33;
+
+    printf("Average: %f\n", (scores1 + scores2 + scores3) / 3.0);
+
+}
