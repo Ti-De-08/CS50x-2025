8fca449e1ccd46a1940b9b03494869e3a583b308
Tirtha Debnath <d.tirtha@op.iitg.ac.in>
2026-01-03 01:53:01 +0530

    Add files via upload

---
+++ Week_4/volume/volume.c
@@ -0,0 +1,47 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    FILE *input = fopen(argv[1], "rb");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "wb");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        fclose(input);
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    int16_t sample;
+    while (fread(&sample, sizeof(int16_t), 1, input) == 1)
+    {
+        sample = (int16_t) (sample * factor);
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
+
+    fclose(input);
+    fclose(output);
+}
